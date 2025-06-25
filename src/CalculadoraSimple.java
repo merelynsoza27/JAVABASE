b1ea5d559a09dce16f48f592c0756a6acde405f0
MerelynSoza <merelyn.soza555@std.uni.edu.ni>
2025-06-24 18:33:51 -0600

    multiplicacion

--- src/CalculadoraSimple.java
+++ src/CalculadoraSimple.java
@@ -18,6 +18,26 @@ public class CalculadoraSimple {
         int resta = a - b;
         System.out.println("Resta: " + resta);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //multiplicacion 
+        int multiplicacion = a * b;
+        System.out.println("multiplicacion:" + multiplicacion);
+
         scanner.close();
 
         
